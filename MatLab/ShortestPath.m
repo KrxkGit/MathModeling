445c48808e84db6f99e30c886ec6e1b88842290e
Krxk <1246924370@qq.com>
2023-08-29 14:08:27 +0800

    MathModeling

--- MatLab/ShortestPath.m
+++ MatLab/ShortestPath.m
@@ -1,13 +1,17 @@
 clear
 clc
 
+%%定义图节点
 s = [1,2,2,1,1,3,3,3,4,6,4,4,5,9,11,12,9,10,11,12,6,8];
 t = [2,3,4,4,5,8,9,4,5,7,7,6,6,11,12,6,10,13,13,13,13,9];
 
-G = graph(s,t);
-myplot = plot(G,"LineWidth",2);
+%%求最小生成树并作图
+G = graph(s,t); %画图
+T = minspantree(G); %最小生成树
+myplot = plot(T,"LineWidth",2);
 
-[Ps,ds] = shortestpath(G,1,13,"Method","unweighted"); %起点到终点
+%%求最短路径
+[Ps,ds] = shortestpath(T,1,13,"Method","unweighted"); %起点到终点
 highlight(myplot,Ps,"EdgeColor",'red');
-[Pm,dm] = shortestpath(G,1,9,"Method","unweighted"); %起点到挖矿
+[Pm,dm] = shortestpath(T,1,9,"Method","unweighted"); %起点到挖矿
 highlight(myplot,Pm,"EdgeColor",'green');
